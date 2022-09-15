8e7bff45d4a5f2d7201f66dc1d657afb5c3aa7e8
Kisizawa <112299224+Kisizawa@users.noreply.github.com>
2022-09-15 10:59:57 +0900

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -5,9 +5,20 @@ public class Hero {
 	String name;
 	int hp;
 	
+	@Override
 	public int hashCode() {
-		return Objects.hash(this.name,this.hp);
+		return Objects.hash(this.hp, this.name);
 	}
-	
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hero other = (Hero) obj;
+		return hp == other.hp && Objects.equals(name, other.name);
+	}
 }
